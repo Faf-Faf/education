ebd4ea0e6227865f0f7653734f66c553e3d43107
Faf-Faf <welma20038@gmail.com>
2018-01-20 01:27:58 -0800

    Make first steps

---
+++ hello.py
@@ -0,0 +1,17 @@
+"""
+Education project.
+
+by Faf-Faf
+welma20038@gmail.com
+
+https://github.com/Faf-Faf/education.git
+
+"""
+name = input("Enter your name: ")
+
+print("Hello", name)
+
+num = float(input("Enter a number: "))
+
+print("Square of", num, "is", num**2)
+print("Square root of", num, "is", num**0.5)
